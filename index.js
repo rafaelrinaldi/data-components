b6b7858415dc06f20849c992678f05e444012b9e
Rafael Rinaldi <rafael@rinaldi.io>
2016-02-11 00:22:30 -0200

    style(lint): fix linter errors
    Fix linter errors on the main script.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+/* global define */
+/* eslint-disable */
 /**@license
  * data-components v{{version}} <https://github.com/rafaelrinaldi/data-components>
  * Released under {{license}} license
  * Author: Rafael Rinaldi <http://rinaldi.io>
  **/
-(function(exports) {
+/* eslint-enable */
+(function (exports) {
   'use strict';
 
   // Component store
@@ -13,24 +16,24 @@
   function $(selector) {
     var lookup = exports.document.querySelectorAll(selector);
     return [].slice.call(lookup);
-  };
+  }
 
   /**
    * Own implementation of `Object.assign` that works in all browsers.
    * Note that it only does shallow copies.
    **/
-  function mixin(target, source) {
+  function mixin(target /* ...sources */) {
     var from;
     var to = target;
     var index = 0;
     var total = arguments.length;
     var hasOwnProperty = Object.prototype.hasOwnProperty;
 
-    while(++index < total){
+    while (++index < total) {
       from = arguments[index];
 
-      if (from != null) {
-        for(var key in from) {
+      if (from !== null) {
+        for (var key in from) {
           if (hasOwnProperty.call(from, key)) {
             to[key] = from[key];
           }
@@ -87,7 +90,7 @@
   function isRegisteredComponent(component) {
     return Object
               .keys(store)
-              .some(function(id) {
+              .some(function (id) {
                 return id === component;
               });
   }
@@ -97,7 +100,7 @@
     var selector = '[data-component]';
 
     // Loop through all `selector` occurrences and bootup components found
-    $(selector).forEach(function(node, index) {
+    $(selector).forEach(function (node, index) {
       /**
       * `dataset` has its own type (`DOMStringMap`) so we convert it to an
       * actual `Object`.
@@ -122,25 +125,25 @@
     }
 
     var sandbox = {
-      get: function(id) {
+      get: function (id) {
         return this[id];
       },
 
-      set: function(id, value) {
+      set: function (id, value) {
         store = register(id, value);
         update(sandbox);
       }
     };
 
-    console.log(store);
-    console.log(store['button']);
     return update(sandbox);
   }
 
   // UMD export
-  if(typeof define === 'function' && define.amd) {
-    define(function() { return components; });
-  } else if(typeof module !== 'undefined' && module.exports) {
+  if (typeof define === 'function' && define.amd) {
+    define(function () {
+      return components;
+    });
+  } else if (typeof module !== 'undefined' && module.exports) {
     module.exports = components;
   } else {
     exports.components = components;
