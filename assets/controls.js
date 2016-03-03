6d93dad616c8065acea6d010c42088c2d53a5595
Rafael Rinaldi <rafael@rinaldi.io>
2016-03-03 12:09:45 -0300

    feat(demo): es6 syntax

--- assets/controls.js
+++ assets/controls.js
@@ -3,63 +3,64 @@
 'use strict';
 
 /**
+ * Helper to remove an input value and set focus to it.
+ */
+const reset = input => {
+  input.focus();
+  input.value = '';
+};
+
+/**
  * =============================================================================
  * Controls
  * =============================================================================
  * This module is responsible for taking care of user actions
  */
+class Controls {
+  constructor(el, options, sandbox) {
+    const buttons = el.querySelectorAll('button');
+    const add = buttons[0];
+    const clean = buttons[1];
+    const remove = buttons[2];
+    const input = el.querySelector('input');
+    const list = sandbox.get('list');
 
-function Controls(node, options, sandbox) {
-  var buttons = node.querySelectorAll('button');
-  var add = buttons[0];
-  var clean = buttons[1];
-  var remove = buttons[2];
-  var input = node.querySelector('input');
-  var list = sandbox.get('list');
+    /**
+     * Listen for the keyboard and submit whatever the current input value is
+     * for the "list" component as a new product.
+     * Reset the input afterwards.
+     */
+    input.addEventListener('keyup', event => {
+      if (event.keyCode === 13) { // 13 → Enter
+        list.add(input.value);
+        reset(input);
+      }
+    });
 
-  /**
-   * Listen for the keyboard and submit whatever the current input value is
-   * for the "list" component as a new product.
-   * Reset the input afterwards.
-   */
-  input.addEventListener('keyup', function (event) {
-    if (event.keyCode === 13) { // 13 → Enter
+    /**
+     * Same action as if the user hits the enter key, but triggered via a button
+     * click.
+     * Will also reset the input afterwards.
+     */
+    add.addEventListener('click', () => {
       list.add(input.value);
       reset(input);
-    }
-  });
+    });
 
-  /**
-   * Same action as if the user hits the enter key, but triggered via a button
-   * click.
-   * Will also reset the input afterwards.
-   */
-  add.addEventListener('click', function () {
-    list.add(input.value);
-    reset(input);
-  });
+    /**
+     * Simply calls the "cleanup" method of "list" public API.
+     */
+    clean.addEventListener('click', () => {
+      list.cleanup();
+    });
 
-  /**
-   * Simply calls the "cleanup" method of "list" public API.
-   */
-  clean.addEventListener('click', function () {
-    list.cleanup();
-  });
-
-  /**
-   * Simply calls the "removeAll" method of "list" public API.
-   */
-  remove.addEventListener('click', function () {
-    list.removeAll();
-  });
-}
-
-/**
- * Convenient function to remove an input value and set focus to it.
- */
-function reset(input) {
-  input.focus();
-  input.value = '';
+    /**
+     * Simply calls the "removeAll" method of "list" public API.
+     */
+    remove.addEventListener('click', () => {
+      list.removeAll();
+    });
+  }
 }
 
 /* eslint-enable */
--- assets/list.js
+++ assets/list.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 'use strict';
 
 /**
@@ -7,77 +9,81 @@
  * This module knows how to translate data into a list output (UI).
  */
 
-function List(node, options) {
-  // Save a reference for the component element
-  this.el = node;
+class List {
+  constructor(el, options) {
+    // Save a reference for the component element
+    this.el = el;
+    // Save a reference for the component options
+    this.options = options;
 
-  // List of strings that represent our products
-  // Read from `options.values` if available
-  this.products = options && options.values ? options.values.split(', ') : [];
+    // List of strings that represent our products
+    // Read from `options.values` if available
+    this.products = options.values.split(', ');
 
-  // Will listen for every click on `<li>` elements and then toggle `is-done`
-  this.el.addEventListener('click', function (event) {
-    if (event.target && event.target.matches('li')) {
-      event.target.classList.toggle('is-done');
+    // Will listen for every click on `<li>` elements and then toggle `is-done`
+    this.el.addEventListener('click', event => {
+      if (event.target && event.target.matches('li')) {
+        event.target.classList.toggle('is-done');
+      }
+    });
+
+    this.render();
+  }
+
+  /**
+   * Add a new product to the products list and then render.
+   */
+  add(product) {
+    if (!product || !product.length) {
+      return;
     }
-  });
 
-  this.render();
+    this.products.push(product);
+    this.render();
+  }
+
+  /**
+   * Cleanup "done" items and then render.
+   */
+  cleanup() {
+    // Look up for all "done" items
+    const done = this.el.querySelectorAll('li.is-done');
+
+    [].slice.call(done).forEach(product => {
+      // Uses the product text content as a keyword and test it against the
+      // products list to see if its a valid index
+      const index = this.products.indexOf(product.textContent);
+
+      // If an index is found use it to remove the element from the list
+      this.products.splice(index, 1);
+
+      // Actually remove the "done" product element from the DOM
+      this.el.removeChild(product);
+    });
+
+    this.render();
+  }
+
+  /**
+   * Empty the products list and then render.
+   */
+  removeAll() {
+    this.products.length = 0;
+    this.render();
+  }
+
+  /**
+   * Render the list to the DOM.
+   */
+  render() {
+    if (this.products.length) {
+      // Loop through all the items on the list and turn them into `<li>`
+      this.el.innerHTML = this.products.map(product => `<li>${product}</li>`).join('');
+    } else {
+      // If list is empty render "Cart is empty"
+      this.el.innerHTML = `<h3>${this.options.empty}</h3>`;
+    }
+  }
 }
 
-/**
- * Add a new product to the products list and then render.
- */
-List.prototype.add = function (product) {
-  if (!product || !product.length) {
-    return;
-  }
-
-  this.products.push(product);
-  this.render();
-};
-
-/**
- * Cleanup "done" items and then render.
- */
-List.prototype.cleanup = function () {
-  // Look up for all "done" items
-  var done = this.el.querySelectorAll('li.is-done');
-
-  [].slice.call(done).forEach(function (product) {
-    // Uses the product text content as a keyword and test it against the
-    // products list to see if its a valid index
-    var index = this.products.indexOf(product.textContent);
-
-    // If an index is found use it to remove the element from the list
-    this.products.splice(index, 1);
-
-    // Actually remove the "done" product element from the DOM
-    this.el.removeChild(product);
-  }.bind(this));
-
-  this.render();
-};
-
-/**
- * Empty the products list and then render.
- */
-List.prototype.removeAll = function () {
-  this.products.length = 0;
-  this.render();
-};
-
-/**
- * Render the list to the DOM.
- */
-List.prototype.render = function () {
-  if (this.products.length) {
-    // Loop through all the items on the list and turn them into `<li>`
-    this.el.innerHTML = this.products.map(function (product) {
-      return '<li>' + product + '</li>';
-    }).join('');
-  } else {
-    // If list is empty render "Cart is empty"
-    this.el.innerHTML = '<h3>Cart is empty</h3>';
-  }
-};
+/* eslint-enable */
